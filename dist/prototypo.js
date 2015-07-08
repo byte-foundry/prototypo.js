98fbe48f196f5bf83b894c51b8e23a6397f98fa2
= <lrbabe@gmail.com>
2015-07-08 11:11:55 +0200

    commit dist files

--- dist/prototypo.js
+++ dist/prototypo.js
@@ -21679,8 +21679,7 @@ Utils.pointOnCurve = function(pointHandleOut,
 			pointHandleIn.point.add(pointHandleIn.handleIn),
 			pointHandleIn.point
 		];
-	}
-	else {
+	} else {
 		points = [
 			pointHandleIn.point,
 			pointHandleIn.point.add(pointHandleIn.handleIn),
